b8494f09f4aadefd8bcfc14674e5a96f8a1b9230
ilikeschecters <hamiltia@mail.gvsu.edu>
2018-08-02 18:06:12 -0400

    added more comments

--- .bin/hashScript.sh
+++ .bin/hashScript.sh
@@ -1,55 +1,52 @@
-cd ..
+cd .. #change directory to the home directory of the project
 PARENT_PATH=$(pwd)
-BIN_PATH=$(echo $PARENT_PATH/.bin)
-HASHF_PATH=$(echo $PARENT_PATH/.hashes)
-
+BIN_PATH=$(echo $PARENT_PATH/.bin) # Path to the script
+HASHF_PATH=$(echo $PARENT_PATH/.hashes) # Path to where the "node files" are
 echo "What is the folder you'd like to hash?"
-read HASH_PATH                                          #gets the path to the folder that will be hashed
-cd $HASH_PATH                                           #changes user to the correct directory
+read HASH_PATH
+cd $HASH_PATH
 echo "Getting list of files and making hashes..."
 
-ls | sed 's/ /\\ /g' | tee $BIN_PATH/files.txt          #put all the names of files in one file
-declare -i NUM_FILES=$(wc -l < $BIN_PATH/files.txt)     #get the number of files
-declare -i CURRENT_FILE=1                               #current file num being iterated thru..
+# The following creates a file with the names of all files being hashed
+ls | sed 's/ /\\ /g' | tee $BIN_PATH/files.txt                        #put all the names of files in one file
+declare -i NUM_FILES=$(wc -l < $BIN_PATH/files.txt)   #get the number of files
+declare -i CURRENT_FILE=1                      #current file num being iterated thru..
 declare -i FILES_HASHED=$NUM_FILES
 
-while read line                                         #loop, while there are still files, it hashes
+# The following block creates a file containing the hash, and moves it to the hash directory
+while read line
 do
-  touch $CURRENT_FILE.txt
-  LINE_HASH=$(cat $line | sha512sum)
-  echo $LINE_HASH | tee $CURRENT_FILE.txt
-  mv $CURRENT_FILE.txt $HASHF_PATH
+  touch $CURRENT_FILE.txt # creates a new file for the given file hash
+  LINE_HASH=$(cat $line | sha512sum) # create hash
+  echo $LINE_HASH | tee $CURRENT_FILE.txt # writes hash to file
+  mv $CURRENT_FILE.txt $HASHF_PATH # moves file to .hashes directory
   CURRENT_FILE=$CURRENT_FILE+1
 done < $BIN_PATH/files.txt
 
-#these four lines are what setup the tree in our program
 
 declare -i CURRENT_LEVEL=0
-declare -i TOP_LEVEL=$(echo "l($NUM_FILES)/l(2)" | bc -l | cut -f1 -d".")
-declare -i FILE_EXP=$(echo "2^$TOP_LEVEL" | bc -l)
-declare -i OVERFLOW=$((NUM_FILES-FILE_EXP))
+declare -i TOP_LEVEL=$(echo "l($NUM_FILES)/l(2)" | bc -l | cut -f1 -d".") # the amount of levels
+declare -i FILE_EXP=$(echo "2^$TOP_LEVEL" | bc -l) # the amount of 2^n nodes
+declare -i OVERFLOW=$((NUM_FILES-FILE_EXP)) # the amount of files that arent 2^n
 
 
-
+# The following block is used to deal with overflow...
+# it works by concatenating the overflow so that there
+# is a 2^n structure
 if [ "$OVERFLOW" -gt 0 ]
 then
 
   CURRENT_HASH=1
-  
-  #while there are more than one files in our folder, it will continue to combine the files and rehash them
-  #this is set up for if there is an odd number
-  
+
   while [ $CURRENT_HASH -lt $((OVERFLOW + 1)) ]
   do
-    NEW_HASH_POS1=$((CURRENT_HASH * 2))
-    NEW_HASH_POS2=$((CURRENT_HASH * 2 - 1))
-    # echo $NEW_HASH_POS1
-    # echo $NEW_HASH_POS2
-    HASH_CONCAT="$(cat $HASHF_PATH/$NEW_HASH_POS1.txt) $(cat $HASHF_PATH/$NEW_HASH_POS2.txt)"
-    rm $HASHF_PATH/$NEW_HASH_POS1.txt
+    NEW_HASH_POS1=$((CURRENT_HASH * 2)) # goes to the second item and creates a hash
+    NEW_HASH_POS2=$((CURRENT_HASH * 2 - 1)) # goes to the item goes to the item before and hashes it
+    HASH_CONCAT="$(cat $HASHF_PATH/$NEW_HASH_POS1.txt) $(cat $HASHF_PATH/$NEW_HASH_POS2.txt)" # concatenate the hashes
+    rm $HASHF_PATH/$NEW_HASH_POS1.txt # remove files from working directory
     rm $HASHF_PATH/$NEW_HASH_POS2.txt
 
-    echo $HASH_CONCAT | sha512sum | tee $HASHF_PATH/$CURRENT_HASH.txt
+    echo $HASH_CONCAT | sha512sum | tee $HASHF_PATH/$CURRENT_HASH.txt # make a new hash file
 
     CURRENT_HASH=$((CURRENT_HASH + 1))
   done
@@ -58,20 +55,20 @@ then
   echo currenthash $CURRENT_HASH
   echo overflow $OVERFLOW
 
+  # the following block moves the hash files that arent overflow to their correct numeric pos
   while [ $CURRENT_HASH -lt $((NUM_FILES + 1)) ]
   do
-    NEW_POS=$((CURRENT_HASH - OVERFLOW))
-    mv $HASHF_PATH/$CURRENT_HASH.txt $HASHF_PATH/$NEW_POS.txt
+    NEW_POS=$((CURRENT_HASH - OVERFLOW)) # calculate what the new position of the hash is
+    mv $HASHF_PATH/$CURRENT_HASH.txt $HASHF_PATH/$NEW_POS.txt # renames file
     echo
     CURRENT_HASH=$((CURRENT_HASH + 1))
   done
 
-  NUM_FILES=$FILE_EXP
+  NUM_FILES=$FILE_EXP # the number of files is now the 2^n value
 
 fi
 
-#does the same thing as loop above, but for even numbers
-
+# The following consolidates the tree to one hash
 while [ $CURRENT_LEVEL -lt $TOP_LEVEL ]
 do
 
@@ -89,8 +86,6 @@ do
 
     echo $HASH_CONCAT | sha512sum | tee $HASHF_PATH/$CURRENT_HASH.txt
 
-
-
     CURRENT_HASH=$((CURRENT_HASH + 1))
   done
 
@@ -99,8 +94,7 @@ do
 
 done
 
-#tests to make sure the hash is correct
-
+# the following is used to check for correctness then ask user if it wants to be stored
 echo checking hash...
 
 clear
@@ -117,11 +111,13 @@ echo RECORDED HASH $CORRECT_HASH
 
 if [ "$FINAL_HASH" == "$CORRECT_HASH" ]
 then
+  echo
   echo "Hashes match!"
 fi
 
 if [ "$FINAL_HASH" != "$CORRECT_HASH" ]
 then
+  echo
   echo "Hashes don't match!"
   echo "Would you like to save this new hash as the new hash (y/n)?"
   read ANSWER
